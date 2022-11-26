5d725fc58230f6204e4df4e777e844e89564c16e
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-11-26 22:16:59 +0900

    feat: add main page view

---
+++ pages/market/main.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Main = () => {
+  return (
+    <section className='w-full h-full p-8 overflow-x-auto scrollbar-hide'>
+      <div className='flex flex-col gap-5'>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        <div className='flex flex-row gap-5'>
+          <div className='w-32 h-32 border rounded-2xl'>
+            <Image src='' alt='' />
+          </div>
+          <div className='w-72 h-32 border rounded-2xl'></div>
+        </div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default Main;
